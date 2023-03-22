2ef27538e64c09edc060ffee9fb63da29de892c7
italoDRocha <italorocha305@gmail.com>
2023-03-22 16:45:09 -0300

    teste mvc bem sucedido

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function listUser()
     {
-        echo "<h1>Listagem de Usuário</h1>";
+        // $user = new User();
+        // $user->name = "Italo";
+        // $user->email = "italo@gmail.com";
+        // $user->password = Hash::make(value:'123');
+        // $user->save();
+        // echo "<h1>Listagem de Usuário</h1>";
+        $user = User::where('id','=',1) ->first();
+        
+        return view('listUser', [
+            'user' => $user
+        ]);
     }
 }
