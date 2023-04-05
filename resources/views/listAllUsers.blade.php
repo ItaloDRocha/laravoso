b35fc4dedf6f0b8ae4b53d982c2d16772b81a5f1
italoDRocha <italorocha305@gmail.com>
2023-04-05 15:26:19 -0300

    lits user com jq

--- resources/views/listAllUsers.blade.php
+++ resources/views/listAllUsers.blade.php
@@ -22,7 +22,11 @@
             <td>{{$user->name}}</td>
             <td>{{$user->email}}</td>
             <td>
-                <a href="">Ver Usuário</a>
+                <form id ='ver_usuario_form{{$user->id}}' action="{{route('users.list', ['user' => $user->id])}}" method="get">
+                    @csrf
+                    <a class='ver_usuario_js' data-user='{{$user->id}}' href="javascript:void(0)">Ver Usuário</a>
+                </form>
+                
                 <form action="{{route('users.destroy', ['user' => $user->id])}}" method="post">
                     @csrf
                     @method('delete')
@@ -33,5 +37,20 @@
         </tr>
         @endforeach
     </table>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            $(".ver_usuario_js").click(function (e) { 
+                let user_id = $(this).attr("data-user");
+
+                $(`#ver_usuario_form${user_id}`).submit();
+            });
+            
+        });
+    </script>
+    
 </body>
 </html>
